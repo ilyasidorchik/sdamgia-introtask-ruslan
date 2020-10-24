903a0f024b71aaf6818fe2842e6d4f6f6ce5dfd8
Ruslan Akseonov <54501083+rusaksenov@users.noreply.github.com>
2020-10-24 17:13:17 +0300

    Show result in Search (#2)

---
+++ src/components/Search/Result/Result.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Result = ({ name, title }) => <div><a href={`https://${name}-ege.sdamgia.ru`}>{title}</a></div>
+
+export default Result
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,10 @@
 import React, { useState, useCallback } from 'react'
 
+import Result from './Result/Result'
+
 const Search = () => {
   const [value, setValue] = useState('')
+  const [result, setResult] = useState(null)
 
   const handleChange = useCallback(
     (e) => {
@@ -10,32 +13,42 @@ const Search = () => {
     []
   )
 
-  const handleClick = useCallback(
-    async () => {
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault()
+
       const fetchData = async () => {
         const url = `https://sdamgia-homework-backend.herokuapp.com/api/search?query=${value}`
         const response = await fetch(url)
         const data = await response.json()
 
-        return data.subject
+        return data ? data.subject : ''
       }
 
       const { name, title } = await fetchData()
-      alert(`${name} - ${title}`)
+
+      setResult({
+        title,
+        name
+      })
+
     },
     [value]
   )
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Searching..."
-        value={value}
-        onChange={handleChange}
-      />
-      <button onClick={handleClick}>Поиск</button>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Searching..."
+          value={value}
+          onChange={handleChange}
+        />
+        <button>Поиск</button>
+      </form >
+      {result && <Result name={result.name} title={result.title} />}
+    </div >
   )
 }
 
