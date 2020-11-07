c8a2900acc6a74dd6359a1615e0055b52db18178
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-11-07 18:53:30 +0300

    Fix Result.js after review

--- src/components/Search/Result/Result.js
+++ src/components/Search/Result/Result.js
@@ -7,7 +7,9 @@ const Result = ({ name, title }) => {
   const cnResult = cn('Result')
 
   return (
-    <div className={cnResult()}><a className={cnResult('Link')} href={`https://${name}-ege.sdamgia.ru`}>{title}</a></div>
+    <div className={cnResult()}>
+      <a className={cnResult('Link')} href={`https://${name}-ege.sdamgia.ru`}>{title}</a>
+    </div>
   )
 }
 export default Result
