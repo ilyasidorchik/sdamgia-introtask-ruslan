3a09e8502768d43077cb99a9066dd59fa977654c
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-11-10 22:29:21 +0300

    Add api.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,53 +2,50 @@ import React, { useState, useCallback } from 'react'
 import { cn } from '@bem-react/classname'
 
 import Result from './Result/Result'
+import api from './api'
 import './Search.scss'
 
 const Search = () => {
   const [value, setValue] = useState('')
   const [result, setResult] = useState(null)
 
-  const handleChange = useCallback(
-    (e) => {
-      setValue(e.target.value);
-    },
-    []
-  )
+  const useHelpers = () => {
+    return {
+      handleChange: useCallback(
+        (e) => {
+          setValue(e.target.value);
+        },
+        []
+      ),
 
-  const handleSubmit = useCallback(
-    async (e) => {
-      e.preventDefault()
+      handleSubmit: useCallback(
+        async (e) => {
+          e.preventDefault()
 
-      const fetchData = async () => {
-        const url = `https://sdamgia-homework-backend.herokuapp.com/api/search?query=${value}`
-        const response = await fetch(url)
-        const data = await response.json()
+          const { name, title } = await api(value)
 
-        return data ? data.subject : ''
-      }
+          setResult({
+            title,
+            name
+          })
+        },
+        [value]
+      )
+    }
+  }
 
-      const { name, title } = await fetchData()
-
-      setResult({
-        title,
-        name
-      })
-
-    },
-    [value]
-  )
-
+  const { handleChange, handleSubmit } = useHelpers()
   const cnSearch = cn("Search")
 
   return (
     <div className={cnSearch()}>
       <form className={cnSearch('Form')} onSubmit={handleSubmit}>
         <input
+          className={cnSearch('Input')}
           type="text"
           placeholder="Searching..."
           value={value}
           onChange={handleChange}
-          className={cnSearch('Input')}
         />
         <button className={cnSearch('Button')}>Поиск</button>
       </form >
---
+++ src/components/Search/api.js
@@ -0,0 +1,9 @@
+const api = async (value) => {
+  const url = `https://sdamgia-homework-backend.herokuapp.com/api/search?query=${value}`
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return data ? data.subject : ''
+}
+
+export default api
