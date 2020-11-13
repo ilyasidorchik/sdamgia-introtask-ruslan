ff7c892a242d4247a9de1506080c72a317dbb925
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-11-13 17:23:48 +0300

    Fix useHelper() after review

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { cn } from '@bem-react/classname'
 
 import Result from './Result/Result'
-import useHelpers from './hooks/useHelpers'
+import { useHelpers } from './hooks/useHelpers'
 import './Search.scss'
 
 const Search = () => {
