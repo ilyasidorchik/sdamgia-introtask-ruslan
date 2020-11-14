7b131073c788cb729ad49892ce7f3222519112e1
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-11-14 10:55:09 +0300

    Add export useHelper() after review

--- src/components/Search/hooks/useHelpers.js
+++ src/components/Search/hooks/useHelpers.js
@@ -2,27 +2,32 @@ import React, { useState, useCallback } from 'react'
 
 import { getSubject } from './api'
 
-export const handleChange = () => {
-  useCallback(
-    (e) => {
-      setValue(e.target.value);
-    },
-    []
-  )
+export const useHelpers = () => {
+  const handleChange = () => {
+    useCallback(
+      (e) => {
+        setValue(e.target.value);
+      },
+      []
+    )
+  }
+
+  const handleSubmit = () => {
+    useCallback(
+      async (e) => {
+        e.preventDefault()
+
+        const { name, title } = await getSubject(value)
+
+        setResult({
+          title,
+          name
+        })
+      },
+      [value]
+    )
+  }
+
+  return { handleChange, handleSubmit }
 }
 
-export const handleSubmit = () => {
-  useCallback(
-    async (e) => {
-      e.preventDefault()
-
-      const { name, title } = await getSubject(value)
-
-      setResult({
-        title,
-        name
-      })
-    },
-    [value]
-  )
-}
