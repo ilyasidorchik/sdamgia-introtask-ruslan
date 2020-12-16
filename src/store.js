f8d364f36f24f5fcfd077a320be345f32479fd42
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-12-16 19:15:13 +0300

    Add extension of Redux

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './modules'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 export default store
