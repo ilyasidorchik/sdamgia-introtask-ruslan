f23f4dd9b552dba8ff0e451fa1650a51f477bb73
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-12-18 01:32:12 +0300

    Use redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
 import rootReducer from './modules'
 
 const store = createStore(
   rootReducer,
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
