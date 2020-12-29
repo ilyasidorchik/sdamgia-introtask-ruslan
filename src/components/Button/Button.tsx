42b80d63d8ba4050d6a7b9244dd3a52471b693b5
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-12-29 10:49:51 +0300

    Add class Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,8 @@
 import React, { ReactNode, FC } from 'react'
+import { cn } from '@bem-react/classname'
+import { classnames as cx } from '@bem-react/classnames'
+
+import './Button.scss'
 
 interface IButtonProps {
   className?: string,
@@ -7,8 +11,10 @@ interface IButtonProps {
 }
 
 const Button: FC<IButtonProps> = ({ className, disabled, children}) => {
+  const button = cn('Button')
+
   return (
-    <button className={className}>{children}</button>
+    <button className={cx(button(), className)}>{children}</button>
   )
 }
 
