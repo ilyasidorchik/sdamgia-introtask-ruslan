c12f64cac03bd7f9a9bcb5108083579df69b9202
Ruslan Aksenov <akseonov.ru@ya.ru>
2021-01-17 20:55:35 +0300

    Create test for Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -5,13 +5,30 @@ import Button from './Button'
 
 describe('Button', () => {
   const  { container } = render(<Button>Найти</Button>)
-  const component = container.querySelector('.Button')
+  
+  describe('common', () => {
+    const component = container.querySelector('.Button')
+    
+    it('renders correctly', () => {
+      expect(component).toHaveTextContent('Найти')
+    })
 
-  it('render correctly', () => {
-    expect(component).toHaveTextContent('Найти')
+    it('matches snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
   })
 
-  it('matches snapshot', () => {
-    expect(component).toMatchSnapshot()
+  describe('with extra className and disabled', () => {
+  
+    const component = container.querySelector('.Button', '.Button_disabled')
+
+    it('renders correctly', () => {
+      expect(component).toHaveTextContent('Найти')
+    })
+
+    it('matches snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
   })
+
 })
