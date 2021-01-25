19b90ab901b4d8f78ade8dab5bab3e262935a912
Ruslan Aksenov <akseonov.ru@ya.ru>
2021-01-25 15:24:25 +0300

    Works with test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 
 import Button from './Button'
+
+let container = null
 
 describe('Button', () => {
   const  { container } = render(<Button>Найти</Button>)
@@ -27,13 +30,34 @@ describe('Button', () => {
       expect(component).toHaveTextContent('Найти')
     })
 
-    it('renders className', () => {
-      expect(component).toHaveClass(component)
+  })
+
+})
+
+describe('Button', () => {
+  const  { container } = render(<Button>Найти</Button>)
+  
+  describe('common', () => {
+    const component = container.querySelector('.Button')
+    
+    it('renders correctly', () => {
+      expect(component).toHaveTextContent('Найти')
     })
 
     it('matches snapshot', () => {
       expect(component).toMatchSnapshot()
     })
+  })
+
+  describe('with extra className and disabled', () => {
+    const  { container } = render(<Button className={this.className}>Найти</Button>)
+  
+    const component = container.querySelector('.Button.Button_disabled')
+
+    it('renders correctly', () => {
+      expect(component).toHaveTextContent('Найти')
+    })
+
   })
 
 })
