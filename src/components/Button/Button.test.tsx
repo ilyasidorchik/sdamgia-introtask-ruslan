ed3af12434a3de660d3f17fff49c68813d0d7b17
Ruslan Aksenov <akseonov.ru@ya.ru>
2021-01-25 21:45:45 +0300

    Fix test for button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,38 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { act } from 'react-dom/test-utils'
 
 import Button from './Button'
-
-let container = null
-
-describe('Button', () => {
-  const  { container } = render(<Button>Найти</Button>)
-  
-  describe('common', () => {
-    const component = container.querySelector('.Button')
-    
-    it('renders correctly', () => {
-      expect(component).toHaveTextContent('Найти')
-    })
-
-    it('matches snapshot', () => {
-      expect(component).toMatchSnapshot()
-    })
-  })
-
-  describe('with extra className and disabled', () => {
-    const  { container } = render(<Button className={this.className}>Найти</Button>)
-  
-    const component = container.querySelector('.Button.Button_disabled')
-
-    it('renders correctly', () => {
-      expect(component).toHaveTextContent('Найти')
-    })
-
-  })
-
-})
 
 describe('Button', () => {
   const  { container } = render(<Button>Найти</Button>)
