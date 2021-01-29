11bd3a352090116829a1ac6a892a2ed1c530bb0c
Ruslan Aksenov <akseonov.ru@ya.ru>
2021-01-29 22:45:28 +0300

    Formatted button and his test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render } from '@testing-library/react'
 
-import Button from './Button';
+import Button from './Button'
 
 describe('Button', () => {
-	describe('common', () => {
-		const { container } = render(<Button>Найти</Button>);
-		const component = container.querySelector('.Button');
+  describe('common', () => {
+    const { container } = render(<Button>Найти</Button>)
+    const component = container.querySelector('.Button')
 
-		it('renders correctly', () => {
-			expect(component).toHaveTextContent('Найти');
-		});
+    it('renders correctly', () => {
+      expect(component).toHaveTextContent('Найти')
+    })
 
-		it('matches snapshot', () => {
-			expect(component).toMatchSnapshot();
-		});
-	});
+    it('matches snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
 
-	describe('with extra className and disabled', () => {
-		const { container } = render(
-			<Button className={'Profile-Button'} disabled={true}>
-				Сохранить
-			</Button>
-		);
-		const component = container.querySelector('.Button.Button_disabled');
+  describe('with extra className and disabled', () => {
+    const { container } = render(
+      <Button className={'Profile-Button'} disabled={true}>
+        Сохранить
+      </Button>
+    )
+    const component = container.querySelector('.Button.Button_disabled')
 
-		it('renders correctly', () => {
-			expect(component).toHaveTextContent('Сохранить');
-		});
+    it('renders correctly', () => {
+      expect(component).toHaveTextContent('Сохранить')
+    })
 
-		it('matches snapshot', () => {
-			expect(component).toMatchSnapshot();
-		});
-	});
-});
+    it('matches snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,26 @@
-import React, { ReactNode, FC } from 'react';
-import { cn } from '@bem-react/classname';
+import React, { ReactNode, FC } from 'react'
+import { cn } from '@bem-react/classname'
 
-import './Button.scss';
+import './Button.scss'
 
 interface IButtonProps {
-	className?: string;
-	disabled?: boolean;
-	children: ReactNode;
+  className?: string
+  disabled?: boolean
+  children: ReactNode
 }
 
 const Button: FC<IButtonProps> = ({
-	className,
-	disabled = false,
-	children,
+  className,
+  disabled = false,
+  children,
 }) => {
-	const cnButton = cn('Button');
+  const cnButton = cn('Button')
 
-	return (
-		<button className={cnButton({ disabled }, [className])} disabled={disabled}>
-			{children}
-		</button>
-	);
-};
+  return (
+    <button className={cnButton({ disabled }, [className])} disabled={disabled}>
+      {children}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
